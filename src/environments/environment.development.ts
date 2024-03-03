1416aae6eca42f369cc586c765f57033a785fa2d
Jakub Tus <jakub@tusovci.sk>
2024-03-03 16:50:03 +0100

    feat(app): added env files

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,6 @@
+import {EnvironmentType} from "../types/environment.type";
+
+export const environment: EnvironmentType = {
+  production: false,
+  apiUrl: 'https://br-fe-assignment.github.io/'
+};
