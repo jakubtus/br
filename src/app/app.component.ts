7abbc6d7d0aabdbc967cc5429c7c0e8cbe7e2505
Jakub Tus <jakub@tusovci.sk>
2024-03-04 04:10:09 +0100

    feat(app): fetch json data and create instance of filter component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,35 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {ChangeDetectorRef, Component, OnInit} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
+import {CustomerEventsService} from "./backend/services/customer-events.service";
+import {CustomerEvent} from "./backend/types/customer-event.type";
+import {CommonModule} from "@angular/common";
+import {FilterComponent} from "./components/filter/filter.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    FilterComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'bloomreach';
+export class AppComponent implements OnInit {
+  title = 'Bloomreach assignment';
+  data?: CustomerEvent[];
+
+  constructor(
+    private customerEventService: CustomerEventsService,
+    private cd: ChangeDetectorRef
+  ) {
+  }
+
+  ngOnInit(): void {
+    this.customerEventService.getEvents().subscribe(response => {
+      this.data = response.events;
+      this.cd.markForCheck();
+    });
+  }
 }
